04e6807f6bee8bc7e40eafe5d21d3eb9bc0296bc
Kiddo <eduardokiddo26@gmail.com>
2025-11-04 11:56:59 -0500

    Add graphics library functions and header guard

--- Libreria.h
+++ Libreria.h
@@ -1,1 +1,83 @@
-//libreria
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <iostream>
+#include <windows.h>
+#include <string.h>
+
+using namespace std;
+
+void gotoxy(int x,int y);
+void cambio_color(int x);
+void mostrarCursor(bool visible);
+void bannerInicio();
+
+void cambio_color(int x){
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),x);
+}
+
+void gotoxy(int x,int y) {
+    HANDLE hcon;  
+    hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+    COORD dwPos;  
+    dwPos.X = x;  
+    dwPos.Y= y;  
+    SetConsoleCursorPosition(hcon,dwPos);
+}   
+
+void mostrarCursor(bool visible) {
+    HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE); 
+    CONSOLE_CURSOR_INFO cci;                        
+    GetConsoleCursorInfo(hCon, &cci);              
+    cci.bVisible = visible;                         
+    SetConsoleCursorInfo(hCon, &cci);              
+}
+
+void bannerInicio() {
+    system("cls");
+    string titulo = "SISTEMA DE GESTION HOSPITALARIA";
+    string subtitulo = "    Hospital Nacional";
+    
+    cambio_color(11);
+    gotoxy(25, 5);
+    cout << char(201);
+    for (int i = 0; i < 40; i++) cout << char(205);
+    cout << char(187);
+
+    for (int i = 6; i <= 11; i++) {
+        gotoxy(25, i); cout << char(186);
+        gotoxy(66, i); cout << char(186);
+    }
+
+    gotoxy(25, 12);
+    cout << char(200);
+    for (int i = 0; i < 40; i++) cout << char(205);
+    cout << char(188);
+
+    cambio_color(14);
+    gotoxy(30, 7);
+    for (char c : titulo) {
+        cout << c;
+        Sleep(80);
+    }
+
+    gotoxy(33, 9);
+    for (char c : subtitulo) {
+        cout << c;
+        Sleep(50);
+    }
+
+    cambio_color(7);
+    gotoxy(35, 11);
+    cout << "Cargando sistema";
+    for (int i = 0; i < 3; i++) {
+        cout << ".";
+        Sleep(700);
+    }
+    Sleep(600);
+
+    system("cls");
+    cambio_color(15);
+}
+
+#endif
